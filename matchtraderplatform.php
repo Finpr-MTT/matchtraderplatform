2704f81845e225c2a726434f7255cf8443d0a72b
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-20 23:31:04 +0800

    Update matchtraderplatform.php

--- matchtraderplatform.php
+++ matchtraderplatform.php
@@ -33,11 +33,7 @@ class MatchTraderPlatform {
      * Constructor to initialize the plugin
      */
     public function __construct() {
-        // Load required files
-        $this->load_helpers();
-        $this->load_admin();
-        $this->load_public();
-
+        $this->load_general_functions();
         // Hook into WordPress
         add_action('plugins_loaded', [$this, 'init']);
         register_activation_hook(__FILE__, [$this, 'activate']);
@@ -45,39 +41,16 @@ class MatchTraderPlatform {
     }
 
     /**
-    * Load helper classes
+    * Load general functions classes
     */
-    private function load_helpers() {
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/helper/class-matchtrader-helper.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/helper/class-matchtrader-enqueue.php';
-    }
-
-    /**
-     * Load admin classes
-     */
-    private function load_admin() {
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/admin/class-matchtrader-functions.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/admin/class-matchtrader-api-settings.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/admin/class-matchtrader-addons-settings.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/admin/class-matchtrader-prctable-settings.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/admin/class-matchtrader-woocommerce-settings.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/admin/class-matchtrader-variable-product-settings.php';
-    }
-
-    /**
-     * Load public-facing classes
-     */
-    private function load_public() {        
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/public/general/class-matchtrader-public-general.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/public/platform-connection/class-matchtrader-api-get-account-by-uuid.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/public/woocommerce/class-matchtrader-public-woocommerce.php';
-        require_once MATCHTRADERPLUGIN_PATH . 'includes/public/woocommerce/class-matchtrader-public-variation-selection.php';
+    private function load_general_functions() {
+        require_once MATCHTRADERPLUGIN_PATH . 'includes/class-matchtrader-general-functions.php';
     }
 
     /**
      * Initialize plugin functionality
      */
-    public function init() {
+    public function init() {        
         MatchTrader_Functions::init();
     }
 
