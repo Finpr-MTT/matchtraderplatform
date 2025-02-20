7bf720a098c73a2724254c76d74efd21ff33f349
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-20 15:07:22 +0800

    Update class-matchtrader-addons-settings.php

--- includes/admin/class-matchtrader-addons-settings.php
+++ includes/admin/class-matchtrader-addons-settings.php
@@ -11,3 +11,34 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+
+class MatchTrader_Addons_Settings{
+
+    /**
+     * Constructor to initialize WooCommerce settings.
+     */
+    public function __construct() {
+        add_action('admin_init', [$this, 'register_settings']);
+    }
+
+    /**
+     * Registers WooCommerce settings in WordPress.
+     */
+    public function register_settings() {
+
+    }
+
+    /**
+     * Renders the WooCommerce settings page.
+     */
+    public static function settings_page() {
+        ?>
+        <div class="wrap">
+            <h1>MTT Addons Settings</h1>
+        </div>
+        <?php
+    }
+}
+
+// Initialize the class instance
+new MatchTrader_Addons_Settings();
