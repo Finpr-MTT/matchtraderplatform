789ab863133026d55c947ceb0bdced4d92743360
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-23 17:04:12 +0800

    Update class-matchtrader-all-style-script.php

--- includes/helper/class-matchtrader-all-style-script.php
+++ includes/helper/class-matchtrader-all-style-script.php
@@ -21,12 +21,19 @@ class MatchTrader_Enqueue_Manager {
     }
 
     public function enqueue_scripts() {
-        if (is_checkout()) {            
+        if (is_checkout()) {
+            // Style
+            wp_enqueue_style('matchtrader-bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css', [], '5.3.0-alpha3' );           
             wp_enqueue_style('matchtrader-platform-style', MATCHTRADERPLUGIN_URL . 'assets/css/matchtraderplatform-public.css', [], MATCHTRADERPLUGIN_VERSION );
             wp_enqueue_style('matchtrader-switch-variant-style', MATCHTRADERPLUGIN_URL . 'assets/css/matchtrader-switch-variant.css', [], MATCHTRADERPLUGIN_VERSION );
+
+            // Script
+            wp_enqueue_script('matchtrader-bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js', ['jquery'], '5.3.0-alpha3', true);
             wp_enqueue_script('matchtrader-platform-script', MATCHTRADERPLUGIN_URL . 'assets/js/matchtraderplatform-public.js', ['jquery'], MATCHTRADERPLUGIN_VERSION, true);            
             wp_enqueue_script('matchtrader-switch-variant', MATCHTRADERPLUGIN_URL . 'assets/js/matchtrader-switch-variant.js', ['jquery'], MATCHTRADERPLUGIN_VERSION, true);
             wp_enqueue_script('matchtrader-woocommerce-script', MATCHTRADERPLUGIN_URL . 'assets/js/matchtraderplatform-woocommerce.js', ['jquery'], MATCHTRADERPLUGIN_VERSION, true);
+
+            // Wp Localize
             wp_localize_script('matchtrader-woocommerce-script', 'wc_country_states', [
                 'countries' => WC()->countries->get_allowed_countries(),
                 'states' => WC()->countries->get_states(),
