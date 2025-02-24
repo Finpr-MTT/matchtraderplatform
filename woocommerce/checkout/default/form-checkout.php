58a1e0adfd5e34156a14cac95f71bbec4611f40c
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-24 12:53:47 +0800

    update core

--- woocommerce/checkout/default/form-checkout.php
+++ woocommerce/checkout/default/form-checkout.php
@@ -53,11 +53,13 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 				
 				<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
+				<?php do_action('matchtrader_checkout_after_order_review');?>
+
 				<div id="order_review" class="woocommerce-checkout-review-order">
 					<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 				</div>
 
-				<?php do_action('matchtrader_checkout_after_order_review');?>
+
 				
 
 			</div>
