158ff74ea26200f7ef1145de0ff3e2c74ef81230
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-25 17:07:11 +0800

    update core

--- woocommerce/checkout/multistep/form-checkout.php
+++ woocommerce/checkout/multistep/form-checkout.php
@@ -54,7 +54,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 			    	<?php do_action('woocommerce_checkout_before_order_review'); ?>	    	
 			    </div>			    
 			</div>
-			<div class="row justify-content-md-center my-5 g-3 shadow-none p-4 bg-light rounded">
+			<div class="row justify-content-md-center my-5 g-3 shadow-none p-4 mtt-bg-button rounded">
 				<div class="col-md-6 py-2">
 			    	<strong>Discount Code</strong>   	
 			    </div>
