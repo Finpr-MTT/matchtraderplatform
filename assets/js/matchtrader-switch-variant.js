c1dbe7ba55b937101297e372147c6c2e573a6efc
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-25 15:14:48 +0800

    Update matchtrader-switch-variant.js

--- assets/js/matchtrader-switch-variant.js
+++ assets/js/matchtrader-switch-variant.js
@@ -52,7 +52,7 @@
                 },
                 success: function (response) {
                     console.log('Order total response:', response);
-                    $('.matchtrader-order-total-value').html('<strong>' + response.order_total + '</strong>').fadeTo(300, 1);
+                    $('.matchtrader-order-total-value').html('<strong>' + response.data.order_total + '</strong>').fadeTo(300, 1);
                 },
                 error: function (xhr, status, error) {
                     console.error('AJAX error:', error);
