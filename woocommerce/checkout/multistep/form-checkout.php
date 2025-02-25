8c2542b293d1b0750264f78e750bbd81ccaca51d
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-25 11:07:25 +0800

    Update form-checkout.php

--- woocommerce/checkout/multistep/form-checkout.php
+++ woocommerce/checkout/multistep/form-checkout.php
@@ -41,21 +41,36 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	<!-- Step 1: Account Selection -->
 	<div class="step-content active" data-step="1">
 		<div class="container">
-		<div class="row justify-content-md-center gap-3">
-		    <div class="col-md-6 border px-4">
-		    	<?php do_action('woocommerce_checkout_before_customer_details'); ?>
-		    </div>
-		    <div class="col-md-5 border px-4">
-		    	<?php do_action('woocommerce_checkout_before_order_review'); ?>
-		    	<div class="border p-3 mt-4">
-		    		<?php do_action('matchtrader_checkout_after_order_review');?>
-		    	</div>
-		    	<div class="mt-4">
-					<button type="button" class="w-100 btn btn-primary next-step">Next</button>
+			<div class="row justify-content-md-center gap-3">
+			    <div class="col-md-12 border px-4">
+			    	<?php do_action('woocommerce_checkout_before_customer_details'); ?>
+			    </div>
+			</div>
+			<div class="row justify-content-md-center gap-3">
+			    <div class="col-md-6 border px-4">
+			    	<?php do_action('woocommerce_checkout_before_order_review'); ?>	    	
+			    </div>
+			    <div class="col-md-6 border px-4">
+			    	<?php do_action('woocommerce_checkout_before_order_review'); ?>	    	
+			    </div>
+			    <div class="col-md-12 border px-4">
+			    	<div class="border p-3 mt-4">
+			    		<?php do_action('matchtrader_checkout_after_order_review');?>
+			    	</div>		    	
+			    </div>
+			</div>
+			<div class="row justify-content-md-center gap-3">
+			    <div class="col-md-12 border px-4">
+			    	<div class="mt-4">
+						<button type="button" class="w-100 btn btn-primary next-step">Next</button>
+					</div>	    	
+			    </div>
+			</div>
+			<div class="row justify-content-md-center gap-3">
+			    <div class="mt-4">
+						<button type="button" class="w-100 btn btn-primary next-step">Next</button>
 				</div>
-		    </div>
-		    
-		</div>
+			</div>		    
 		</div>
 	</div>
 
