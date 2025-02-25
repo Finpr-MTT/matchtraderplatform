07875f066b8cb07dc3f8598477b3c065c1e7d83a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-25 15:18:12 +0800

    Update form-checkout.php

--- woocommerce/checkout/multistep/form-checkout.php
+++ woocommerce/checkout/multistep/form-checkout.php
@@ -45,21 +45,18 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 			    <div class="col-md-12">
 			    	<?php do_action('woocommerce_checkout_before_customer_details'); ?>
 			    </div>
-			</div>
-			<div class="row justify-content-md-center g-3">
+			    <div class="clearfix"></div>
 			    <div class="col-md-6">
 			    	<?php do_action('woocommerce_checkout_before_order_review'); ?>	    	
 			    </div>
 			    <div class="col-md-6">
 			    	<?php do_action('woocommerce_checkout_before_order_review'); ?>	    	
 			    </div>			    
-			</div>
-			<div class="row justify-content-md-center">
+			    <div class="clearfix"></div>
 				<div class="col-md-12 text-center">
 			    	<?php do_action('matchtrader_checkout_display_price_order');?>    	
 			    </div>
-			</div>
-			<div class="row justify-content-md-center">
+			    <div class="clearfix"></div>
 			    <div class="col-md-4">
 			    	<div class="mt-4">
 						<button type="button" class="w-100 btn btn-primary next-step">Next</button>
