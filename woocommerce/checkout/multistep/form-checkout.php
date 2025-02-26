4a0d389540fc19503baccc8b6dbecfc4c4bc2b3b
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-26 13:26:41 +0800

    Update form-checkout.php

--- woocommerce/checkout/multistep/form-checkout.php
+++ woocommerce/checkout/multistep/form-checkout.php
@@ -101,9 +101,9 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 					<div id="customer_details">
 						<?php do_action( 'woocommerce_checkout_billing' ); ?>
 					</div>
-					<div class="d-flex justify-content-between my-5 gap-sm-3">
+					<div class="d-flex justify-content-between my-5">
 						<button type="button" class="p-3 btn mtt-bg-button-previous w-25 prev-step">Back</button>
-						<button type="button" class="p-3 btn mtt-bg-button-next w-75 w-md-25 next-step">Next</button>
+						<button type="button" class="p-3 btn mtt-bg-button-next w-25 next-step">Next</button>
 					</div>
 				</div>
 			</div>
