f19521a583eccae62fcb8e30bf2e693e2edc1118
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-27 13:49:57 +0800

    Update matchtraderplatform-addons.js

--- assets/js/matchtraderplatform-addons.js
+++ assets/js/matchtraderplatform-addons.js
@@ -27,35 +27,10 @@
             });
         }
 
-        // Restore checked checkboxes on page load
-        function restoreCheckedAddons() {
-            $.ajax({
-                type: 'POST',
-                url: mtt_addons_ajax.ajax_url,
-                data: {
-                    action: 'get_selected_addons',
-                    nonce: mtt_addons_ajax.nonce
-                },
-                success: function (response) {
-                    if (response.success && response.addons) {
-                        $('input[type="checkbox"][name="mtt_addons[]"]').each(function () {
-                            let addonName = $(this).next('label').text().trim();
-                            if (response.addons.includes(addonName)) {
-                                $(this).prop('checked', true);
-                            }
-                        });
-                    }
-                }
-            });
-        }
-
         // Event listener for checkbox changes
         $('input[type="checkbox"][name="mtt_addons[]"]').on('change', function () {
             updateSelectedAddons();
         });
-
-        // Restore checkboxes on page load
-        restoreCheckedAddons();
     });
 
 })(jQuery);
